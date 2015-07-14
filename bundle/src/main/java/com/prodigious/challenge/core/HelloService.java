bcc41ee6f2192f1791ac84c72dc404e68c6e9117
David Cuellar <jorge.cuellar@gmail.com>
2015-07-14 17:18:34 -0500

    Initial structure of the project.

---
+++ bundle/src/main/java/com/prodigious/challenge/core/HelloService.java
@@ -0,0 +1,13 @@
+package com.prodigious.challenge.core;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
